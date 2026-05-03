611a82548f24256a3f42a4e5d9ed2f20b52501bc
Leonmmcoset <152147508+Leonmmcoset@users.noreply.github.com>
2026-05-03 09:44:26 +0000

    style: auto-format C/C++ sources with clang-format

--- kernel/runtime/user.c
+++ kernel/runtime/user.c
@@ -189,16 +189,14 @@ static void clks_user_store_be32(u32 value, u8 *out) {
 
 static void clks_user_sha256_transform(clks_user_sha256_ctx *ctx, const u8 data[64]) {
     static const u32 k[64] = {
-        0x428A2F98U, 0x71374491U, 0xB5C0FBCFU, 0xE9B5DBA5U, 0x3956C25BU, 0x59F111F1U, 0x923F82A4U,
-        0xAB1C5ED5U, 0xD807AA98U, 0x12835B01U, 0x243185BEU, 0x550C7DC3U, 0x72BE5D74U, 0x80DEB1FEU,
-        0x9BDC06A7U, 0xC19BF174U, 0xE49B69C1U, 0xEFBE4786U, 0x0FC19DC6U, 0x240CA1CCU, 0x2DE92C6FU,
-        0x4A7484AAU, 0x5CB0A9DCU, 0x76F988DAU, 0x983E5152U, 0xA831C66DU, 0xB00327C8U, 0xBF597FC7U,
-        0xC6E00BF3U, 0xD5A79147U, 0x06CA6351U, 0x14292967U, 0x27B70A85U, 0x2E1B2138U, 0x4D2C6DFCU,
-        0x53380D13U, 0x650A7354U, 0x766A0ABBU, 0x81C2C92EU, 0x92722C85U, 0xA2BFE8A1U, 0xA81A664BU,
-        0xC24B8B70U, 0xC76C51A3U, 0xD192E819U, 0xD6990624U, 0xF40E3585U, 0x106AA070U, 0x19A4C116U,
-        0x1E376C08U, 0x2748774CU, 0x34B0BCB5U, 0x391C0CB3U, 0x4ED8AA4AU, 0x5B9CCA4FU, 0x682E6FF3U,
-        0x748F82EEU, 0x78A5636FU, 0x84C87814U, 0x8CC70208U, 0x90BEFFFAU, 0xA4506CEBU, 0xBEF9A3F7U,
-        0xC67178F2U};
+        0x428A2F98U, 0x71374491U, 0xB5C0FBCFU, 0xE9B5DBA5U, 0x3956C25BU, 0x59F111F1U, 0x923F82A4U, 0xAB1C5ED5U,
+        0xD807AA98U, 0x12835B01U, 0x243185BEU, 0x550C7DC3U, 0x72BE5D74U, 0x80DEB1FEU, 0x9BDC06A7U, 0xC19BF174U,
+        0xE49B69C1U, 0xEFBE4786U, 0x0FC19DC6U, 0x240CA1CCU, 0x2DE92C6FU, 0x4A7484AAU, 0x5CB0A9DCU, 0x76F988DAU,
+        0x983E5152U, 0xA831C66DU, 0xB00327C8U, 0xBF597FC7U, 0xC6E00BF3U, 0xD5A79147U, 0x06CA6351U, 0x14292967U,
+        0x27B70A85U, 0x2E1B2138U, 0x4D2C6DFCU, 0x53380D13U, 0x650A7354U, 0x766A0ABBU, 0x81C2C92EU, 0x92722C85U,
+        0xA2BFE8A1U, 0xA81A664BU, 0xC24B8B70U, 0xC76C51A3U, 0xD192E819U, 0xD6990624U, 0xF40E3585U, 0x106AA070U,
+        0x19A4C116U, 0x1E376C08U, 0x2748774CU, 0x34B0BCB5U, 0x391C0CB3U, 0x4ED8AA4AU, 0x5B9CCA4FU, 0x682E6FF3U,
+        0x748F82EEU, 0x78A5636FU, 0x84C87814U, 0x8CC70208U, 0x90BEFFFAU, 0xA4506CEBU, 0xBEF9A3F7U, 0xC67178F2U};
     u32 m[64];
     u32 a;
     u32 b;
@@ -497,8 +495,7 @@ static clks_bool clks_user_parse_record(const char *line, struct clks_user_recor
     if (uid_text != CLKS_NULL) {
         clks_user_trim(uid_text);
     }
-    if (clks_user_name_valid(name) == CLKS_FALSE || clks_strlen(hash) != CLKS_USER_HASH_HEX_LEN ||
-        home[0] != '/') {
+    if (clks_user_name_valid(name) == CLKS_FALSE || clks_strlen(hash) != CLKS_USER_HASH_HEX_LEN || home[0] != '/') {
         return CLKS_FALSE;
     }
 
@@ -589,7 +586,8 @@ static clks_bool clks_user_find(const char *name, struct clks_user_record *out_r
     return CLKS_FALSE;
 }
 
-static clks_bool clks_user_verify_password(const char *name, const char *password, struct clks_user_record *out_record) {
+static clks_bool clks_user_verify_password(const char *name, const char *password,
+                                           struct clks_user_record *out_record) {
     struct clks_user_record record;
     char hash[CLKS_USER_HASH_HEX_LEN + 1U];
 
@@ -875,8 +873,7 @@ clks_bool clks_user_at(u64 index, struct clks_user_public_info *out_info) {
     u64 i;
     u64 current = 0ULL;
 
-    if (CLKS_CFG_USER_SYSTEM == 0 || out_info == CLKS_NULL ||
-        clks_user_read_db(db, sizeof(db), &len) == CLKS_FALSE) {
+    if (CLKS_CFG_USER_SYSTEM == 0 || out_info == CLKS_NULL || clks_user_read_db(db, sizeof(db), &len) == CLKS_FALSE) {
         return CLKS_FALSE;
     }
 
@@ -994,8 +991,7 @@ clks_bool clks_user_path_read_allowed(const char *path) {
         return CLKS_FALSE;
     }
 
-    if (CLKS_CFG_USER_SYSTEM == 0 || CLKS_CFG_USER_READ_POLICY == 0 ||
-        clks_user_disk_login_required == CLKS_FALSE) {
+    if (CLKS_CFG_USER_SYSTEM == 0 || CLKS_CFG_USER_READ_POLICY == 0 || clks_user_disk_login_required == CLKS_FALSE) {
         return CLKS_TRUE;
     }
 
@@ -1036,8 +1032,7 @@ clks_bool clks_user_path_write_allowed(const char *path) {
         return CLKS_FALSE;
     }
 
-    if (CLKS_CFG_USER_SYSTEM == 0 || CLKS_CFG_USER_WRITE_POLICY == 0 ||
-        clks_user_disk_login_required == CLKS_FALSE) {
+    if (CLKS_CFG_USER_SYSTEM == 0 || CLKS_CFG_USER_WRITE_POLICY == 0 || clks_user_disk_login_required == CLKS_FALSE) {
         return CLKS_TRUE;
     }
 
